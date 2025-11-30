6e68b87d47ce44bd857176318f15fb14777ed28d
Anastasia <3540455@bk.ru>
2025-11-30 23:16:56 +0300

    add block for the score, container for the grid, paly buttons; add style blocks for these elements

--- js/script.js
+++ js/script.js
@@ -14,9 +14,45 @@ function createTitle(app) {
     app.appendChild(title);
 }
 
+function createScore(app) {
+    const scoreBlock = document.createElement("div");
+    scoreBlock.id = 'score';
+    scoreBlock.textContent = "Score: 0";
+    app.appendChild(scoreBlock);
+    return scoreBlock;
+}
+
+function createGridContainer(app) {
+    const gridContainer = document.createElement("div");
+    gridContainer.id = 'grid-container';
+    app.appendChild(gridContainer);
+    return gridContainer;
+}
+
+function createButtons(app) {
+    const gameButtons = document.createElement("div");
+    gameButtons.id = 'game-buttons';
+
+    const btnNew = document.createElement("button");
+    btnNew.textContent = "New Game";
+
+    const btnDelete = document.createElement("button");
+    btnDelete.textContent = "Delete";
+
+    const btnLeaderboard = document.createElement("button");
+    btnLeaderboard.textContent = "Leaderboard";
+
+    gameButtons.append(btnNew, btnDelete, btnLeaderboard)
+
+    app.appendChild(gameButtons);
+}
+
 function init() {
     const app = createAppContainer();
     createTitle(app);
+    createScore(app);
+    createGridContainer(app);
+    createButtons(app);
 }
 
 init()
